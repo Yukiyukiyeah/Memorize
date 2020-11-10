ccc031f70ec90b9bcb126e188417a1ed1c73a719
Yuki <yukitang@mail.bnu.edu.cn>
2020-11-10 13:17:17 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Yuki Tang on 2020/11/10.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
